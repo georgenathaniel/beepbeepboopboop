02a213063cb536860d529b056a7f1bb8b35d6db4
georgenathaniel <22gnathaniel@gannacademy.org>
2022-02-17 16:51:19 -0500

    yes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobottimeFAST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobottimeFAST.java
@@ -6,20 +6,48 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class RobottimeFAST extends TankDrive {
+   // private Servo servo;
+   // private double position;
+    private double extendpower;
+    private double updownpower;
+
 
     @Override
     public void init() {
         super.init();
-
+       // servo = hardwareMap.get(Servo.class, "Servo");
+        //position = 0.5;
+        extendpower = 0;
+        updownpower = 0;
     }
 
     public void loop() {
         super.loop();
-        double extendpower = gamepad1.left_trigger;
-        double updownpower = gamepad1.right_trigger;
 
         extend.setPower(extendpower);
-        updown.setPower(extendpower);
+        if (gamepad1.dpad_right) {
+            extendpower = 0.25;
+        }
+        if (gamepad1.dpad_left) {
+            extendpower = -0.25;
+        }
+
+        updown.setPower(updownpower);
+        if (gamepad1.dpad_up) {
+            updownpower = 0.25;
+        }
+        if (gamepad1.dpad_down) {
+            updownpower = -0.25;
+        }
+
+       /* servo.setPosition(position);
+        if (gamepad1.a) {
+            position -= 0.0005;
+        }
+        if (gamepad1.b) {
+            position += 0.0005;
+        }*/
+
     }
 }
 
